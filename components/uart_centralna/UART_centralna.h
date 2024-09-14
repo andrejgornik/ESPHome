82bda0f2bfcf43de9ec23c99cf25a1d5698b0524
Andrej Gornik <andrej.gornik@gmail.com>
2024-09-15 01:16:34 +0200

    Popravki

--- components/uart_centralna/UART_centralna.h
+++ components/uart_centralna/UART_centralna.h
@@ -1,30 +1,32 @@
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
+#include "esphome/components/text_sensor/text_sensor.h"  // Include text_sensor
 
 namespace esphome {
 namespace uart_centralna {
 
 class MyCustomUARTComponent : public uart::UARTDevice, public Component {
  public:
-  // Modify the constructor to accept a UARTComponent pointer
-  MyCustomUARTComponent(UARTComponent *parent) : UARTDevice(parent) {}
+  // Use fully qualified namespace for UARTComponent
+  MyCustomUARTComponent(uart::UARTComponent *parent) : uart::UARTDevice(parent) {}
 
   void setup() override {}
   void loop() override;
   void dump_config() override;
-  
+
   // Method to create and return text sensors
   text_sensor::TextSensor *create_text_sensor(const std::string &name) {
     auto *sensor = new text_sensor::TextSensor();
-    sensor->set_name(name);
+    // Use c_str() to pass a const char* to set_name
+    sensor->set_name(name.c_str());
     text_sensors_.push_back(sensor);
     return sensor;
   }
 
  protected:
   std::string buffer_;
-  std::vector<text_sensor::TextSensor *> text_sensors_;
+  std::vector<text_sensor::TextSensor *> text_sensors_;  // Use fully qualified namespace
 };
 
 }  // namespace uart_centralna
