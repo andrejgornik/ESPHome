bef438e582770c6f603565445a5ae5bf28d92227
Andrej Gornik <andrej.gornik@gmail.com>
2024-09-15 00:18:37 +0200

    Popravki

--- components/UART_centralna/__init__.py
+++ components/UART_centralna/__init__.py
@@ -1,22 +1,19 @@
-# This is an initialization file for the UART_centralna component
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import pins
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-# Define the namespace and class in ESPHome
+# Define the namespace
 uart_centralna_ns = cg.esphome_ns.namespace('uart_centralna')
 MyCustomUARTComponent = uart_centralna_ns.class_('MyCustomUARTComponent', cg.Component, uart.UARTDevice)
 
-# Define configuration schema (if needed)
+# Define configuration schema
 CONFIG_SCHEMA = uart.UART_DEVICE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(MyCustomUARTComponent),
 })
 
 # Register the component
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID], cg.global_ns.UARTComponent)
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
