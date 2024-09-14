8f1377afe87821763a16e00bd4f55eeb99f5585b
Andrej Gornik <andrej.gornik@gmail.com>
2024-09-15 01:05:56 +0200

    Popravki

--- components/uart_centralna/UART_centralna.h
+++ components/uart_centralna/UART_centralna.h
@@ -7,12 +7,24 @@ namespace uart_centralna {
 
 class MyCustomUARTComponent : public uart::UARTDevice, public Component {
  public:
+  // Modify the constructor to accept a UARTComponent pointer
+  MyCustomUARTComponent(UARTComponent *parent) : UARTDevice(parent) {}
+
   void setup() override {}
   void loop() override;
-  void dump_config() override;  // Declaration only
+  void dump_config() override;
+  
+  // Method to create and return text sensors
+  text_sensor::TextSensor *create_text_sensor(const std::string &name) {
+    auto *sensor = new text_sensor::TextSensor();
+    sensor->set_name(name);
+    text_sensors_.push_back(sensor);
+    return sensor;
+  }
 
  protected:
   std::string buffer_;
+  std::vector<text_sensor::TextSensor *> text_sensors_;
 };
 
 }  // namespace uart_centralna
